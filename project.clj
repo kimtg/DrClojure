3bcad8a2877ecbbbeae067b29ce6d668143f401d
Kim, Taegyoon <steloflute@gmail.com>
2014-03-27 18:36:06 +0900

    v0.1.10: uses Clojure 1.6.0

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject DrClojure "0.1.9"
+(defproject DrClojure "0.1.10"
   :description "DrClojure"
   :url "http://steloflute.tistory.com/entry/DrClojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]]
   :main DrClojure.core)
