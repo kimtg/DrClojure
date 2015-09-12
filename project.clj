8f886fcfc79729136f86d3591b42a6acbbe79ca2
KIM Taegyoon <steloflute@gmail.com>
2015-09-13 02:13:28 +0900

    clojure v1.7.0

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject DrClojure "0.1.16"
+(defproject DrClojure "0.1.17"
   :description "DrClojure"
   :url "http://steloflute.tistory.com/entry/DrClojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]]
   :main DrClojure.core
   :aot :all)
